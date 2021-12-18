566652aeb60bab06d9315d1b27a2a738dcaaccd4
jon <jonathanharvey22@gmail.com>
2021-12-17 19:59:25 -0500

    completed project 3

--- 3/main.js
+++ 3/main.js
@@ -1,1 +1,19 @@
+// Largest prime factor of 600851475143?
 
+var primeFactors = [1];
+var found = false;
+
+function factorPrimes(number){
+    let start = primeFactors[primeFactors.length - 1] + 1;
+    for (let i = start; i < number; i ++ ){
+        if( number % i == 0){
+            primeFactors.push(i);
+            break;
+        }
+    }
+}
+factorPrimes(600851475143);
+factorPrimes(600851475143);
+factorPrimes(600851475143);
+factorPrimes(600851475143);
+console.log(primeFactors);
