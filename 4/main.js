adfe94bb629d26fe4561f8e45efc1676ad7ce431
jon <jonathanharvey22@gmail.com>
2021-12-18 17:27:11 -0500

    solved project 4

--- 4/main.js
+++ 4/main.js
@@ -1,1 +1,36 @@
+// largest product of two 3-digit numbers that results in a palindrome
 
+let palindromes = [];
+let largestPair = [0, 0];
+
+function checkPalindrome(number){
+    let stringNumber = number.toString();
+    let numberSplitArray = stringNumber.split("");
+    let numberSplitArrayReversed = numberSplitArray.reverse();
+    let numberReversedJoined = numberSplitArrayReversed.join("");
+    if (numberReversedJoined == number) {
+        return "palindrome"
+    } else {
+        return "non-palindrome";
+    }
+}
+function palindromeChecker(numberone, numbertwo){
+    let product = numberone * numbertwo;
+    let productChecked = checkPalindrome(product);
+    if (productChecked == "palindrome"){
+        let numberSet = [numberone, numbertwo];
+        palindromes.push(numberSet);
+    }
+    else{
+
+    }
+}
+function numberPairGenerator(){
+    for (let i = 600; i < 1000; i ++){
+        for (let j = 600; j < 1000; j ++){
+            palindromeChecker(i, j);
+        }
+    }
+}
+numberPairGenerator();
+console.log(palindromes[palindromes.length - 1]);
