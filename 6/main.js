d3c6e8a3aa9939616075a713c62e13dd308ca432
Jon <63463014+collectivenectar@users.noreply.github.com>
2021-12-19 12:54:20 -0500

    initial commit for #6

---
+++ 6/main.js
@@ -0,0 +1,4 @@
+//The sum of the squares of the first ten natural numbers is 1^2 + 2^2 + 3^2, + ... = 385
+//The square of the sum of the first ten natural numbers is (1 + 2 + 3 + ...)^2 = 3025
+// 3025 - 385 = 2640
+//Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum
