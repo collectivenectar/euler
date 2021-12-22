0baa35b234e163aa3033b258e71e6a473132c275
jon <jonathanharvey22@gmail.com>
2021-12-22 12:54:41 -0500

    project #10 completed

--- 10/main.js
+++ 10/main.js
@@ -1,2 +1,24 @@
 //The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 //Find the sum of all the primes below two million.
+
+var primeNumbers = [2, 3, 5, 7];
+var primeSum = 17;
+
+
+function primeChecker(limit){
+    for (let i = 11; i <= limit - 1; i ++){
+        for (let j = 0; j <= primeNumbers.length - 1; j++){
+            if (i % primeNumbers[j] == 0){
+                break;
+            }
+            else {
+                if (j == primeNumbers.length - 1){
+                    primeNumbers.push(i);
+                    primeSum += i;
+                }
+            }
+        }
+    }
+}
+primeChecker(2000000);
+console.log(primeSum);
